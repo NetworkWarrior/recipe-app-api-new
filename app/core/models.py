326a035e9a27055d7ac5249dc3984369880a41d6
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2023-07-24 12:13:50 +0200

    normilize email adress

--- app/core/models.py
+++ app/core/models.py
@@ -5,12 +5,14 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create, save and return a new user"""
-        user=self.model(email=email, **extra_fields)
+        """Create, save and return a new user."""
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -19,10 +21,12 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
-    email=models.EmailField(max_length=255, unique=True)
-    name=models.CharField(max_length=255)
-    is_active=models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=False)
-    objects=UserManager()
 
-    USERNAME_FIELD='email'
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
