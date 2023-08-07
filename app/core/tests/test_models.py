e01e041574c53172925f237de04211d2e84eed93
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2023-08-07 22:38:27 +0200

    Writing test for recipe model (should fail)

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,7 +4,8 @@ Test for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
+from decimal import Decimal
+from core import models
 class TestModels(TestCase):
     """Test Models."""
 
@@ -44,3 +45,17 @@ class TestModels(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe_succes(self):
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user = user,
+            title = 'sample recipe title',
+            time_minute = 5,
+            price = Decimal('5,50'),
+            description = 'recipe description',
+        )
+        self.assertEqual(str(recipe), recipe.title)
