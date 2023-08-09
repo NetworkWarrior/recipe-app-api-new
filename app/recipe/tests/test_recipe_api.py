632b9d6ccea2ca737f17965a84e77dc2ce21e8a0
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2023-08-09 23:08:32 +0200

    Implementing create recipe API

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -96,3 +96,18 @@ class PrivateRecipeAPITests(TestCase):
         res = self.client.get(url)
         seriailizer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, seriailizer.data)
+
+    def test_create_recipe(self):
+        """Test creating a recipe."""
+        payload = {
+            'title': 'Sample recipe',
+            'time_minutes': 30,
+            'price': Decimal('5.90'),
+        }
+        res = self.client.post(RECIPE_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        for k, v in payload.items():
+            self.assertEqual(getattr(recipe, k), v)
+        self.assertEqual(recipe.user, self.user)
